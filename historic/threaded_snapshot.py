1b81c0bc514507fe749afc645561629154251d0c
Dylan Fontana <dylan.fontana@ge.com>
2017-07-20 22:27:48 -0500

    Optimized thread count, stitch files after every wave to reduce temp files.

--- historic/threaded_snapshot.py
+++ historic/threaded_snapshot.py
@@ -1,5 +1,7 @@
 """ This is a script for obtaining time series data from GDAX """
 import datetime
+import os
+import shutil
 import csv
 import threading
 from itertools import zip_longest
@@ -10,7 +12,6 @@ import gdax
 
 TODO:
     - Make the parts folder if its not on disk
-    - Move header file out of the parts folder into the main directory.
     - Concatenate all files of a wave into the HEADER file after they finish
     and before starting the next wave.
         - Consider doing it every X number of waves, wiping the parts folder after
@@ -22,14 +23,14 @@ TODO:
 
 CLIENT = gdax.PublicClient()
 GRANULARITY = 1 # second
-WAVE_SIZE = 100
+WAVE_SIZE = 7
 
 def main(srttime=None, endtime=None):
     """
     Breaks down the given time period into digestable request "chunks" that
     the GDAX API can process. Outputs results into a CSV file.
     """
-    with open('part_header.csv', 'w') as the_file:
+    with open('part_master.csv', 'w') as the_file:
         writer = csv.writer(the_file, dialect='excel')
         writer.writerow(['time', 'low', 'high', 'open', 'close', 'volume'])
 
@@ -60,13 +61,20 @@ def main(srttime=None, endtime=None):
     # Unleash the threads
     print(str(len(threads)) + " threads constructed.")
     print("Unleashing the Kraken (In waves)...")
+    
+    if not os.path.exists("parts"):
+        os.makedirs("parts")
+    out_file = open("part_master.csv", "a")
+
     wave_index = 1
+    wave_size = 0
     for group in grouper(WAVE_SIZE, threads):
         print("\tStarting Wave " + str(wave_index) + "/" + str(len(threads)))
         for thr in group:
             if thr is None:
                 continue
             else:
+                wave_size += 1
                 thr.start()
 
         for thr in group:
@@ -74,10 +82,26 @@ def main(srttime=None, endtime=None):
                 continue
             else:
                 thr.join()
+
+        print("\tStitching the masts")
+        write_parts_to_master(out_file)
         wave_index += 1
 
     # Seal the deal.
+    out_file.close()
     print("The Seas Have Settled.")
+
+def write_parts_to_master(out_file):
+    """
+    Writes the current contents of the parts directory to the master csv.
+    After which, it deletes the parts & rebuilds folder for the next wave.
+    """
+    for filename in os.listdir("parts"):
+        with open("parts/"+filename) as part:
+            for line in part:
+                out_file.write(line)
+    shutil.rmtree("parts")
+    os.makedirs("parts")
 
 def process_time_frame(start_frame, end_frame, thread_count):
     """
@@ -96,6 +120,7 @@ def process_time_frame(start_frame, end_frame, thread_count):
                 break
             row[0] = datetime.datetime.fromtimestamp(row[0]).strftime('%x %X')
             writer.writerow(row)
+
 
 def grouper(chunk_size, iterable, fillvalue=None):
     """
