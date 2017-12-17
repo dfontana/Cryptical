c1c6a2aa6c6e16fba8ca26681fa6106915757e00
Dylan Fontana <dyfontana@gmail.com>
2017-12-17 16:10:52 -0500

    Added examples to main

--- main.go
+++ main.go
@@ -2,20 +2,36 @@ package main
 
 import (
 	pol "./poloniex"
-	// gdax "./gdax"
+	gdax "./gdax"
 	"time"
 )
 
 func main() {
-	// p := pol.Poloniex{false, []string{"USDT_ETH"}}
-	// recsP := p.Historic("USDT_ETH", time.Date(2017, time.December, 14, 0, 0, 0, 0, time.Local), time.Now())
-	// p.CSV("./outP.csv", recsP)
+	gdaxLive();
+}
 
-	// g := gdax.GDAX{false, []string{"ETH-USD"}}
-	// recs := g.Historic("ETH-USD", time.Date(2017, time.December, 14, 0, 0, 0, 0, time.Local), time.Now(), 200)
-	// g.CSV("./outG.csv", recs)
+func polHist(){
+	p := pol.Poloniex{false, []string{"USDT_ETH"}}
+	recsP := p.Historic("USDT_ETH", time.Date(2017, time.December, 14, 0, 0, 0, 0, time.Local), time.Now())
+	p.CSV("./outP.csv", recsP)
+}
+
+func gdaxHist(){
+	g := gdax.GDAX{false, []string{"ETH-USD"}}
+	recs := g.Historic("ETH-USD", time.Date(2017, time.December, 14, 0, 0, 0, 0, time.Local), time.Now(), 200)
+	g.CSV("./outG.csv", recs)
+}
+
+func polLive(){
 	p := pol.Poloniex{true, []string{"USDT_ETH"}}
-	p.Live()
+	go p.Live()
 	time.Sleep(10 * time.Second)
 	p.Enabled = false
+}
+
+func gdaxLive(){
+	g := gdax.GDAX{true, []string{"ETH-USD"}}
+	go g.Live()
+	time.Sleep(5 * time.Second)
+	g.Enabled = false
 }
