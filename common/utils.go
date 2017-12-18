f13d1ce08b80a7e07538eef0c39def3a7354bd62
Dylan Fontana <dyfontana@gmail.com>
2017-12-18 12:38:35 -0500

    Manually formatting strings fixes error?

--- common/utils.go
+++ common/utils.go
@@ -2,69 +2,34 @@ package common
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"net/http/httputil"
+	"encoding/json"
+	"io/ioutil"
 
 	//CSV creation
 	"encoding/csv"
 	"os"
 )
 
-func SimpleGet(url2 string, into interface{}) error {
-	// res, err := http.Get(url)
-	// if err != nil {
-	// 	return err
-	// }
-
-	// if res.StatusCode != 200 {
-	// 	return fmt.Errorf("SimpleGet returned status %d", res.StatusCode)
-	// }
-
-	// defer res.Body.Close()
-
-	// // Parse the response
-	// if err := json.NewDecoder(res.Body).Decode(&into); err != nil {
-	// 	return err
-	// }
-
-	// return nil
-	var response *http.Response
-	var request *http.Request
-
-	// values := url.Values{}
-	// values.Set("start", "2017-11-18T07:03:18Z")
-	// values.Set("end", "2017-12-18T07:03:18Z")
-	// values.Set("granularity", "86400")
-	// url2 = "https://api.gdax.com/products/ETH-USD/candles?" + values.Encode()
-	log.Println(url2)
-	request, err := http.NewRequest("GET", url2, nil)
-	if err == nil {
-		request.Header.Add("User-Agent", "curl/7.54.0")
-		request.Header.Add("Accept", "*/*")
-		debug(httputil.DumpRequestOut(request, true))
-		response, err = (&http.Client{}).Do(request)
-	}
-
-	if err == nil {
-		defer response.Body.Close()
-		debug(httputil.DumpResponse(response, true))
-		_, err = ioutil.ReadAll(response.Body)
-	}
-
+func SimpleGet(url string, into interface{}) error {
+	res, err := http.Get(url)
 	if err != nil {
-		log.Fatalf("ERROR: %s", err)
+		return err
 	}
-	return nil
-}
 
-func debug(data []byte, err error) {
-	if err == nil {
-		fmt.Printf("%s\n\n", data)
-	} else {
-		log.Fatalf("%s\n\n", err)
+	if res.StatusCode != 200 {
+		err, _ := ioutil.ReadAll(res.Body)
+		return fmt.Errorf("SimpleGet returned status %d, %s", res.StatusCode, string(err))
 	}
+
+	defer res.Body.Close()
+
+	// Parse the response
+	if err := json.NewDecoder(res.Body).Decode(&into); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // WriteToCSV is a routine that will write incoming items to a CSV
