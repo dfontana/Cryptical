89bba1cd754a68d54eb7eb5d8844951a16b84e86
Dylan Fontana <dyfontana@gmail.com>
2018-01-06 22:03:13 -0500

    Introduced a basic example

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>
+}
+
+const element = <Welcome name="Cryptical" />
+ReactDOM.render(element, document.getElementById('root'))
